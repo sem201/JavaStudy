b34feb7df892cc9900e05952efc6c03b49ade2d8
sem201 <sem201@naver.com>
2024-11-22 20:54:08 +0900

    인프런 강의 섹션2(주석)

---
+++ src/CommentJava.java
@@ -0,0 +1,6 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java1"); //hello java출력
+        System.out.println("dd");
+    }
+}
---
+++ src/HelloJava2.java
@@ -0,0 +1,8 @@
+public class HelloJava2 {
+    public static void main(String[] args){
+        System.out.println("gdgd");
+        System.out.println("gdgd2");
+        System.out.println("gdgd3");
+        System.out.println("gdgd4");
+    }
+}
